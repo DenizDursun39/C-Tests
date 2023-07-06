c72c737d12732b6dd9042eeef76838f28b37437f
Denizhan Dursun <86263468+DenizDursun39@users.noreply.github.com>
2023-07-06 14:41:51 +0300

    Müşteri Takip Simülasyonu
    Devam ettiğim kurs için yaptığım müşteri takip simülasyonunun kodları.

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriTakip
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+
+        public string customerName { get; set; }
+
+        public double customerSocialSecNum { get; set; }
+
+
+    }
+}
---
+++ CustomerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriTakip
+{
+    public class CustomerManager
+    {   
+        public CustomerManager() { 
+          
+        }
+        public Customer addCustomer(string customerName,int customerId,double customerSocialSecNum)
+        {
+            Customer customer = new Customer();
+            customer.customerId = customerId;
+            customer.customerName = customerName;
+            customer.customerSocialSecNum = customerSocialSecNum;
+            return customer;
+        }
+        public Customer removeCustomer(Customer customer)
+        {
+            customer.customerName = null;
+            customer.customerSocialSecNum = 0;
+            customer.customerId = 0;
+            return customer = null;
+        }
+        public void listCustomers(params Customer[] customer)
+        {
+            foreach (Customer cust in customer)
+            {
+                Console.WriteLine("Müşterinin adı: " + cust.customerName + "\n" + "Müşteri ID: " + cust.customerId + "\n" + "Müşteri sosyal güvenlik numarası: " + cust.customerSocialSecNum);
+            }
+        }
+
+
+
+    }
+}
