3d7acbfd0ebf75a410f77e358099e10ae3a446b8
Denizhan Dursun <86263468+DenizDursun39@users.noreply.github.com>
2023-07-11 20:03:45 +0300

    8.hafta ödev
    car rental projesinin 8. haftaya güncellenmiş hali

---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBrandDal : IBrandDal
+    {
+        public void Add(Brands entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State=EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Brands entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Brands Get(Expression<Func<Brands, bool>> filter)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return context.Set<Brands>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Brands> GetAll(Expression<Func<Brands, bool>> filter = null)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return filter == null
+                    ? context.Set<Brands>().ToList() :
+                    context.Set<Brands>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Brands entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var modifiedEntity = context.Entry(entity);
+                modifiedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Colors entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Colors entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Colors Get(Expression<Func<Colors, bool>> filter)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return context.Set<Colors>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Colors> GetAll(Expression<Func<Colors, bool>> filter = null)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                return filter == null 
+                    ? context.Set<Colors>().ToList() 
+                    : context.Set<Colors>().Where(filter).ToList();
+                    
+            }
+        }
+
+        public void Update(Colors entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var modifiedEntity = context.Entry(entity);
+                modifiedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
