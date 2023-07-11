2df0e07184311295e270a255eab378af5562d2ce
Denizhan Dursun <86263468+DenizDursun39@users.noreply.github.com>
2023-07-11 20:09:30 +0300

    8.hafta ödevi
    rental car programının güncellenmiş hali

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll()) {
+                Console.WriteLine("{0} {1} {2} {3}",car.Description,car.ModelYear,car.CarId,car.DailyPrice);
+            }
+        }
+    }
+}
